ae2b6f797e09cc265e3d50f365416ab5212545c3
9tc <38623525+9tc@users.noreply.github.com>
2021-07-27 19:22:04 +0900

    Add Boot camp for Beginners(Easy ~29, AtCoder Problems)

---
+++ ABC114/C.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9 //10^9:∞
+#define LLINF (long long)1e12
+#define MOD (int)1e9+7 //10^9+7:合同式の法
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VVI vector<vector<int> > //VVI a(n, vector<int>(m));
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+using namespace atcoder;
+
+int ctoi(char c){
+	return c - '0';
+}
+
+ll n;
+
+ll sft(ll x, bool s, bool f, bool t){
+  ll r = 0;
+  if(x > n) return 0;
+  r += sft(x * 10 + 7, true, f, t);
+  r += sft(x * 10 + 5, s, true, t);
+  r += sft(x * 10 + 3, s, f, true);
+  if(s && f && t) {
+    ++r;
+  }
+  return r;
+}
+
+signed main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> n;
+  cout << sft(0, false, false, false) << endl;
+}
---
+++ hitachi2020/B.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9 //10^9:∞
+#define LLINF (long long)1e12
+#define MOD (int)1e9+7 //10^9+7:合同式の法
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VVI vector<vector<int> > //VVI a(n, vector<int>(m));
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+using namespace atcoder;
+
+
+signed main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+	ll a, b, m;
+	cin >> a >> b >> m;
+	vector<ll> r(a);
+	ll minR = LLINF;
+	REP(i,a) {
+		cin >> r[i];
+		chmin (minR, r[i]);
+	}
+	vector<ll> d(b);
+	ll minD = LLINF;
+	REP(i,b) {
+		cin >> d[i];
+		chmin(minD, d[i]);
+	}
+	ll minPair = minR+minD;
+	ll x, y, c;
+	REP(i,m){
+		cin >> x >> y >> c;
+		chmin(minPair, r[x-1] + d[y-1] - c);
+	}
+	cout << minPair << endl;
+
+}
