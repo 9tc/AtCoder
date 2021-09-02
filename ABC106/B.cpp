575cda5a6b97f4acd2ea472f18b87af8d2f80a86
dokaraya <38623525+9tc@users.noreply.github.com>
2021-09-02 11:35:10 +0900

    Add 全探索sample

---
+++ ABC106/B.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+//#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9 //10^9:∞
+#define LLINF (long long)1e12
+#define MOD (int)1e9+7 //10^9+7:合同式の法
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VVI vector<vector<int> > //VVI a(n, vector<int>(m));
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+//using namespace atcoder;
+
+int yakusu(int n){
+	int cnt = 0;
+	FOR(i,1,n){
+		if(n % i == 0) ++cnt;
+	}
+	return cnt;
+}
+
+signed main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+	int n; cin >> n;
+	int ans = 0;
+	for(int i = 1; i <= n; i += 2){
+		if(yakusu(i) == 8) ++ans;
+	}
+
+	cout << ans << endl;
+}
