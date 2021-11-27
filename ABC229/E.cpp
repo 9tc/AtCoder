ae35235eaece84b74aa4554f8ea7312c21a4ed13
9tc <38623525+9tc@users.noreply.github.com>
2021-11-27 23:01:24 +0900

    add ABC229 (and more)

---
+++ ABC229/E.cpp
@@ -0,0 +1,125 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9 //10^9:∞
+#define LLINF (long long)(1LL<<62)-1
+#define MOD (int)1e9+7 //10^9+7:合同式の法
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VVI vector<vector<int> > //VVI a(n, vector<int>(m));
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+	return a;
+	else
+	return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+using namespace atcoder;
+
+struct UnionFind {
+    vector<int> par;
+
+    UnionFind(int n) : par(n, -1) { }
+    void init(int n) { par.assign(n, -1); }
+
+    int root(int x) {
+        if (par[x] < 0) return x;
+        else return par[x] = root(par[x]);
+    }
+
+    bool issame(int x, int y) {
+        return root(x) == root(y);
+    }
+
+    bool merge(int x, int y) {
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (par[x] < par[y]) swap(x, y);
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+
+    int size(int x) {
+        return -par[root(x)];
+    }
+};
+
+bool f(PII a, PII b){
+	if(a.S < b.S) return true;
+	else if(a.S == b.S) return a.F < b.F;
+	else return false;
+}
+
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	VVI g(n);
+
+	int u, v;
+	REP(i,m) {
+		cin >> u >> v;
+		--u; --v;
+		g[u].PB(v);
+	}
+
+	UnionFind uf(n);
+	VI ans;
+	ans.PB(0);
+
+	int cnt = 0;
+	REPR(i, n){
+		if(i == 0)break;
+		++cnt;
+		for(auto j: g[i]){
+			if(uf.merge(i, j)) --cnt;
+		}
+		ans.PB(cnt);
+	}
+
+	reverse(ALL(ans));
+	REP(i,ans.size()) cout << ans[i] << endl;
+}
