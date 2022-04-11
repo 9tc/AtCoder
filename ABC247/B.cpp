fcb6cda68a2438842410d34ca985ded04d1f3abf
9tc <38623525+9tc@users.noreply.github.com>
2022-04-11 13:59:58 +0900

    add ABC247

---
+++ ABC247/B.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9
+#define LLINF (long long)1e12
+#define MOD (int)1e9+7
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VVI vector<vector<int> > //VVI a(n, vector<int>(m));
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+using namespace atcoder;
+
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+	int n;
+  cin >> n;
+  map<string, int> mp;
+  vector<pair<string, string>> vp(n);
+  REP(i,n){
+    string s, t;
+    cin >> s >> t;
+    vp[i] = {s, t};
+    if(s == t) mp[s]++;
+    else{
+      mp[s]++;
+      mp[t]++;
+    }
+  }
+
+  bool ans = true;
+  REP(i,n){
+    if(mp[vp[i].F] >= 2 && mp[vp[i].S] >= 2) {
+      ans = false;
+      break;
+    }
+  }
+  YesNo(ans);
+}
