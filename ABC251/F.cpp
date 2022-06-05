2ff265190e47f82180228543341e6ba23865b8bf
9tc <38623525+9tc@users.noreply.github.com>
2022-06-05 15:25:37 +0900

    add ABC251

---
+++ ABC251/F.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9
+#define LLINF (long long)1e12
+#define MOD (int)1e9+7
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VVI vector<vector<int> > //VVI a(n, vector<int>(m));
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+using namespace atcoder;
+
+vector<bool> seen;
+void dfs(const Graph &G, int v) {
+    seen[v] = true; // v を訪問済にする
+
+    // v から行ける各頂点 next_v について
+    for (auto next_v : G[v]) {
+        if (seen[next_v]) continue; // next_v が探索済だったらスルー
+        cout << v+1 << " " << next_v+1 << endl;
+        dfs(G, next_v); // 再帰的に探索
+    }
+}
+
+void bfs(const Graph &G, int v) {
+  seen[v] = true;
+  VI vec;
+    // v から行ける各頂点 next_v について
+    for (auto next_v : G[v]) {
+        if (seen[next_v]) continue; // next_v が探索済だったらスルー
+        seen[next_v] = true;
+        cout << v+1 << " " << next_v+1 << endl;
+        vec.PB(next_v);
+    }
+    for (auto next_v : vec) {
+      bfs(G, next_v); // 再帰的に探索
+    }
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  Graph g(n);
+  REP(i,m){
+    int u, v;
+    cin >> u >> v;
+    --u, --v;
+    g[u].PB(v);
+    g[v].PB(u);
+  }
+  seen.assign(n, false);
+  dfs(g, 0);
+  seen.assign(n, false);
+  bfs(g, 0);
+}
