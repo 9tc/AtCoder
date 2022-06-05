47a5e80159bc73384bb536fe1dfaaafbc58b4d5f
9tc <38623525+9tc@users.noreply.github.com>
2022-06-05 15:19:49 +0900

    add ABC250

---
+++ ABC250/D.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9
+#define LLINF (long long)1e12
+#define MOD (int)1e9+7
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VVI vector<vector<int> > //VVI a(n, vector<int>(m));
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+using namespace atcoder;
+
+
+//エラトステネスの篩
+std::vector<bool> IsPrime;
+void sieve(size_t max){
+    if(max+1 > IsPrime.size()){     // resizeで要素数が減らないように
+        IsPrime.resize(max+1,true); // IsPrimeに必要な要素数を確保
+    }
+    IsPrime[0] = false; // 0は素数ではない
+    IsPrime[1] = false; // 1は素数ではない
+
+    for(size_t i=2; i*i<=max; ++i) // 0からsqrt(max)まで調べる
+        if(IsPrime[i]) // iが素数ならば
+            for(size_t j=2; i*j<=max; ++j) // (max以下の)iの倍数は
+                IsPrime[i*j] = false;      // 素数ではない
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  ll n;
+  cin >> n;
+  sieve(1000000);
+  vector<ll> primes;
+  REP(i,1000000){
+    if(i * i * i * 2 > n) break;
+    if(IsPrime[i]) primes.PB(i);
+  }
+
+  ll ans = 0;
+  REP(i, primes.size()){
+    if(i == 0) continue;
+    ll q = primes[i];
+    ll l = 0, r = i;
+    while(r - l > 1){
+      ll mid = (l + r) / 2;
+      ll p = primes[mid];
+      if(q * q * q <= n/ p) l = mid;
+      else r = mid;
+    }
+    ans += l+1;
+  }
+  cout << ans << endl;
+}
---
+++ ABC250/G.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using ll = long long;
+#define REP(i, n) for (ll i = 0; (i) < ll(n); ++ (i))
+#define FOR(i, m, n) for (ll i = (m); (i) <= ll(n); ++ (i))
+#define REPR(i, n) for (ll i = ll(n) - 1; (i) >= 0; -- (i))
+#define FOR3R(i, m, n) for (ll i = ll(n) - 1; (i) >= ll(m); -- (i))
+#define ALL(x) x.begin(),x.end()
+
+#define INF (int)1e9
+#define LLINF (long long)1e18
+#define MOD (int)1e9+7
+#define PI 3.141592653589
+#define PB push_back
+#define F first
+#define S second
+
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(T) if(T){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+#define Graph vector<vector<int> >
+#define PII pair<int,int>
+#define VI vector<int>
+#define VL vector<ll>
+#define VVI vector<vector<int> >
+#define VVL vector<vector<ll> >
+#define VPII vector<pair<int,int> >
+
+#define DDD fixed<<setprecision(10)
+
+template <class T>
+inline bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+inline bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+template <class T>
+T GCD(T a, T b) {
+	if (b == 0)
+		return a;
+	else
+		return GCD(b, a % b);
+}
+template <class T>
+inline T LCM(T a, T b) {
+	return (a * b) / GCD(a, b);
+}
+
+using namespace std;
+using namespace atcoder;
+
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  priority_queue<int, VI, greater<int>> pq;
+  ll ans = 0;
+  while(n--){
+    int t;
+    cin >> t;
+    ans -= t;
+    REP(_,2) pq.push(t);
+    pq.pop();
+  }
+
+  while(!pq.empty()){
+    ans += pq.top();
+    pq.pop();
+  }
+  cout << ans << endl;
+}
